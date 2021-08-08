6a3d7aa6f67e192e97b7d346f8e316b49d4be03a
Alaestor <alaestor@null.net>
2021-08-08 11:48:36 -0400

    1.3 but for real this time

---
+++ Patches/PatchGeneratePawnRestrictionOptions.cs
@@ -0,0 +1,75 @@
+﻿using AIRobot;
+using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace MiscRobotsPlusPlus.Patches
+{
+    // Add robots to bill target list
+    public class PatchGeneratePawnRestrictionOptions
+    {
+        public static void Patch(Harmony harmony)
+        {
+            var nestedTypes = typeof(Dialog_BillConfig).GetNestedTypes(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
+            var targetType = nestedTypes.FirstOrDefault(t => t.FullName.Contains("GeneratePawnRestrictionOptions"));
+            var targetMethod = targetType.GetMethod("MoveNext", BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
+            harmony.Patch(targetMethod, transpiler: new HarmonyMethod(typeof(PatchGeneratePawnRestrictionOptions), nameof(Transpiler)));
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var originalCall = typeof(PawnsFinder).GetMethod("get_AllMaps_FreeColonists");
+            var getColonists = typeof(ColonistsAndRobotsFinder).GetMethod("GetColonistsAndRobots");
+            var bill = typeof(Dialog_BillConfig).GetField("bill", BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Call && instruction.Calls(originalCall))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldloc_2);
+                    yield return new CodeInstruction(OpCodes.Ldfld, bill);
+                    yield return new CodeInstruction(OpCodes.Call, getColonists);
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+
+    public class ColonistsAndRobotsFinder
+    {
+        public static List<Pawn> GetColonistsAndRobots(Bill bill)
+        {
+            var originalTab = Activator.CreateInstance(typeof(X2_MainTabWindow_Robots)) as MainTabWindow_PawnTable;
+            var originalPawnsFunc = typeof(X2_MainTabWindow_Robots).GetProperty("Pawns", BindingFlags.Instance | BindingFlags.NonPublic);
+            var robots = (originalPawnsFunc.GetValue(originalTab) as IEnumerable<Pawn>)
+                ?.Select(r => r as X2_AIRobot)
+                ?.ToList();
+            var ret = PawnsFinder.AllMaps_FreeColonists;
+            if (robots == null) return ret;
+
+            var workGiver = bill?.billStack?.billGiver?.GetWorkgiver();
+            if (workGiver != null)
+            {
+                // Check directly against source def since Misc. Robots doesn't initialize def2 until the robot is activated
+                robots = robots
+                    .Where(r => r.def is X2_ThingDef_AIRobot def2
+                        && (def2.robotWorkTypes?.Any(rwt => rwt.workTypeDef == workGiver.workType) ?? false))
+                    .ToList();
+            }
+            else
+            {
+                Log.Message("Misc. Robots++: Couldn't find workGiver in GeneratePawnRestrictionOptions, skipping filtering of robots");
+            }
+
+            ret.AddRange(robots);
+            return ret;
+        }
+    }
+}
---
+++ Patches/PatchJobDriver.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using Verse;
+using Verse.AI;
+
+namespace MiscRobotsPlusPlus.Patches
+{
+    [HarmonyPatch(typeof(JobDriver), "ReadyForNextToil")]
+    public class PatchJobDriver_ReadyForNextToil
+    {
+        // Unforbid resources that were mined outside of home area
+        static void Prefix(JobDriver __instance)
+        {
+            var pos = __instance?.job?.targetA.Cell;
+            var pawnNamespace = __instance?.pawn?.GetType()?.Namespace;
+            if (pos.HasValue && pos.Value.IsValid
+                && __instance is JobDriver_Mine
+                && pawnNamespace == "AIRobot"
+                && __instance.pawn.Map != null)
+            {
+                var things = pos.Value.GetThingList(__instance.pawn.Map);
+                var forbidden = things
+                    .Where(t => t.IsForbidden(Faction.OfPlayer))
+                    .ToList();
+                forbidden.ForEach(f => f.SetForbidden(false, false));
+            }
+        }
+    }
+
+
+
+    [HarmonyPatch(typeof(WorkGiver_Scanner), "HasJobOnThing")]
+    public class PatchShit
+    {
+        static void Postfix(WorkGiver_Scanner __instance, Pawn pawn, Thing t)
+        {
+            var a = 0;
+        }
+    }
+}
