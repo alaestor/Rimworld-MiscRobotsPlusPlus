8627eda6e16c8d55c8a465b0bb5fad8a72af6731
VeteranXT <103519488+VeteranXT@users.noreply.github.com>
2022-11-22 09:00:04 +0100

    Update to add mod Settings

---
+++ Source/MiscModsSettings.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using MiscRobotsPlusPlus.Patches;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace MiscRobotsPlusPlus
+{
+    public class MiscModsSettings : ModSettings
+    {
+        internal static float tier_1_ConstrctionSpeed = 1f;
+        internal static float tier_1_DeepDrillSpeed = 1f;
+        internal static List<ThingDef> database;
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref tier_1_ConstrctionSpeed, "tier_1_ConstrctionSpeed", 1f);
+        }
+    }
+}
--- Source/MiscRobotsPlusPlus.cs
+++ Source/MiscRobotsPlusPlus.cs
@@ -1,12 +1,15 @@
 ﻿using HarmonyLib;
 using MiscRobotsPlusPlus.Patches;
+using RimWorld;
+using System.Linq;
 using System.Reflection;
+using UnityEngine;
 using Verse;
 
 namespace MiscRobotsPlusPlus
 {
     [StaticConstructorOnStartup]
-    public class MiscRobotsPlusPlus
+    public class MiscRobotsPlusPlus : Mod
     {
         static MiscRobotsPlusPlus()
         {
@@ -14,5 +17,53 @@ namespace MiscRobotsPlusPlus
             harmony.PatchAll(Assembly.GetExecutingAssembly());
             PatchGeneratePawnRestrictionOptions.Patch(harmony);
         }
+
+        public MiscRobotsPlusPlus(ModContentPack content) : base(content)
+        {
+            LongEventHandler.ExecuteWhenFinished(GetSettings);
+            LongEventHandler.ExecuteWhenFinished(PushDatabase);
+        }
+        private void PushDatabase()
+        {
+            MiscModsSettings.database = DefDatabase<ThingDef>.AllDefsListForReading;
+        }
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+        } 
+        public override string SettingsCategory()
+        {
+            return "MiscRobots";
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard guiStandard = new Listing_Standard() 
+            {
+                ColumnWidth = inRect.width
+            };
+ 
+            guiStandard.Begin(inRect);
+            guiStandard.Label("MISC_WIP" );
+            guiStandard.Label("MISC_GlobalTeirs");
+            guiStandard.Label("MISC_Tier1"); 
+            Rect rect = guiStandard.GetRect(Text.LineHeight);
+            Rect rect2 = rect.LeftHalf().Rounded();
+            Rect rect3 = rect.RightHalf().Rounded();
+
+            //Builders
+            
+           
+            //Add Ability To translat later
+       
+            
+            guiStandard.End();
+
+            base.DoSettingsWindowContents(inRect);
+        }
+        void GetSettings()
+        {
+            GetSettings<MiscModsSettings>();
+        }
+
     }
 }
